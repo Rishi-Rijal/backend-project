55e945baa5ba46196e63ab5ba4d8975d7802aafe
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-09-27 14:25:58 +0200

    added custom api error & response and async handler

--- src/index.js
+++ src/index.js
@@ -1,37 +1,28 @@
-// require("dotenv").config({path: "./env"})
+
 import dotenv from "dotenv"
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import express from "express";
 import connnectDB from "./db/index.js";
-
-
+import { app } from "./app.js";
 
 dotenv.config({
-    path:"./db"
+    path: "./db"
 })
 
-connnectDB();
+connnectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Error on app", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        });
+    })
+    .catch(err => {
+        console.log("Mongodb Connection failed !!!", err)
+    });
 
 
 
 
-// ;( async ()=>{
-//     try{
-//         await mongoose.connect(`${process.env.MNGODB_URI}/${DB_NAME}`);
-//         console.log("Database connected successfully");
-//         app.on("error",(error)=>{
-//             console.log("ERRR: ", error);
-//             throw error;
-//         });
 
-//         app.listen(process.env.PORT, ()=>{
-//             console.log(`app listening to port ${process.env.PORT}`);
-//         })
-        
-
-//     }catch(err){
-//         console.log("Error connecting database", err);
-//         throw err;
-//     }
-// })()
